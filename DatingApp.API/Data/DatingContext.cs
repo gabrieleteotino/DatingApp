b8dec09c30fbaf79b99ffc3fc6af0b37adb3a000
Gabriele Teotino <gabriele@teosoft.it>
2018-05-31 00:29:51 +0200

    Webapi initial commit.

---
+++ DatingApp.API/Data/DatingContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingContext : DbContext
+    {
+        public DatingContext(DbContextOptions<DatingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
