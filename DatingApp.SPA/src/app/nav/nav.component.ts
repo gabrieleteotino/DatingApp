5c37359be34e6898f39dc1cff936cea2b9e25e3a
Gabriele Teotino <gabriele@teosoft.it>
2018-06-18 13:53:38 +0200

    Redirect on login and logout.

--- DatingApp.SPA/src/app/nav/nav.component.ts
+++ DatingApp.SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) {}
 
   ngOnInit() {}
 
@@ -19,13 +20,16 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(data => {
       this.alertify.success('Sucessfully logged in');
     }, error => {
-      this.alertify.error(error);
+      this.alertify.error('Failed to login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
   logout() {
     this.authService.logout();
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
