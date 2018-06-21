18e8b62b4334f4c7dee6de2443aa04fe3d10b13f
Gabriele Teotino <gabriele@teosoft.it>
2018-06-21 17:56:38 +0200

    Added member-card.

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { AuthGuard } from './_guards/auth.guard';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
+import { ROUTES } from './routes';
+
+import { AuthService } from './_services/auth.service';
+import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
+
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { RouterModule } from '@angular/router';
-import { ROUTES } from './routes';
-import { AuthGuard } from './_guards/auth.guard';
-import { JwtModule } from '@auth0/angular-jwt';
-import { UserService } from './_services/user.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +31,8 @@ import { UserService } from './_services/user.service';
     RegisterComponent,
     MemberListComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
